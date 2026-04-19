09bcb5cd0e480f795a17b7810c2eaf532eea330c
kazakov7 <kazakov28082003@gmail.com>
2026-04-19 17:40:30 +0900

    EJS orqali ananaviy front end

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ const express = require("express");
 const app = express();
 const http = require("http");
 
-//1:kirish code
+//1: kirish
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//2:session
-//3: views code
+//2: session
+//3:views
 app.set("views", "views");
 app.set("view engine", "ejs");
+
 //4: routing
-app.get("/hello", function (req, res) {
-  res.end("<h1>Hello world</h1>");
+app.post("/create-item", (req, res) => {
+  console.log(req);
+  res.json({ test: "succes" });
 });
-app.get("/gift", function (req, res) {
-  res.end("Siz sovg'alar bo'limidasiz.");
+
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`The running successfully this server port: ${PORT}`);
+  console.log(`The server is succesfully running. Port: ${PORT}`);
 });
---
+++ train.js
@@ -0,0 +1,1 @@
+console.log("Train fayl ishga tushdi");
