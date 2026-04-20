0b43ba2d9f02dd4945ac892468852a6086a15107
kazakov7 <kazakov28082003@gmail.com>
2026-04-21 07:21:54 +0900

    feat: develop author router

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 //1: kirish
 app.use(express.static("public"));
@@ -12,13 +23,17 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4: routing
-app.post("/create-item", (req, res) => {
-  console.log(req);
+app.post("/create-item", function (req, res) {
   res.json({ test: "succes" });
 });
+app.get("/author", function (req, res) {
+  res.render("author", { user: user });
+});
+
 app.get("/", function (req, res) {
   res.render("harid");
 });
+
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
