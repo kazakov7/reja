63a6272f7dbfd329558c9e99abb010be18d5d895
kazakov7 <kazakov28082003@gmail.com>
2026-04-27 07:53:11 +0900

    feat: 23,24,25 darslar va MIT_TASK-B yakunlandi

--- train.js
+++ train.js
@@ -1,3 +1,14 @@
+//MIT TASK-B
+function countNum(word) {
+  let count = 0;
+  for (let letter of word) {
+    if (letter >= 0) {
+      count++;
+    }
+  }
+  console.log(`Ushbu string ichidan ${count} ta raqam topildi`);
+}
+countNum("ad2a54y79wet0sfgb9");
 // //MIT TASK "A"
 // let hisob = 0;
 // function countLetter(a, word) {
